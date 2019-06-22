a4b0fc4f8ed3de50edbf669d7b4351454c080c80
Amine Hilaly <hilalyamine@gmail.com>
2019-06-23 00:52:30 +0200

    Add test cases

--- bridge/github/export_test.go
+++ bridge/github/export_test.go
@@ -1,26 +1,81 @@
 package github
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"math/rand"
+	"net/http"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/MichaelMure/git-bug/bridge/core"
+	"github.com/MichaelMure/git-bug/bug"
 	"github.com/MichaelMure/git-bug/cache"
 	"github.com/MichaelMure/git-bug/repository"
 	"github.com/MichaelMure/git-bug/util/interrupt"
-	"github.com/stretchr/testify/require"
 )
 
+const (
+	testRepoBaseName = "git-bug-test-github-exporter"
+)
+
+// testCases creates bugs in repo cache
+func testCases(repo *cache.RepoCache) (map[string]*cache.BugCache, error) {
+	author, err := repo.NewIdentity("test identity", "hello@testidentity.org")
+	if err != nil {
+		return nil, err
+	}
+
+	bugs := make(map[string]*cache.BugCache)
+
+	// simple bug
+	simpleBug, err := repo.NewBugRaw(author, time.Now().Unix(), "simple bug", "new bug", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	bugs["simple bug"] = simpleBug
+
+	// bug with comments
+	bugWithComments, err := repo.NewBugRaw(author, time.Now().Unix(), "bug with comments", "new bug", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = bugWithComments.AddCommentRaw(author, time.Now().Unix(), "new comment", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	bugs["bug with comments"] = bugWithComments
+
+	// bug with label changes
+	bugLabelChange, err := repo.NewBugRaw(author, time.Now().Unix(), "bug label change", "new bug", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	_, _, err = bugLabelChange.ChangeLabelsRaw(author, time.Now().Unix(), []string{"bug", "core"}, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	_, _, err = bugLabelChange.ChangeLabelsRaw(author, time.Now().Unix(), nil, []string{"bug"}, nil)
+	if err != nil {
+		return nil, err
+	}
+	bugs["bug change label"] = bugWithComments
+
+	return nil, err
+}
+
 func TestExporter(t *testing.T) {
-	//TODO test strategy
-	tests := []struct {
-		name string
-	}{
-		{
-			name: "bug creation",
-		},
+	user := os.Getenv("TEST_USER")
+	token := os.Getenv("BOT_TOKEN")
+	if token == "" {
+		t.Skip("Env var GITHUB_TOKEN_PRIVATE missing")
 	}
 
 	repo := repository.CreateTestRepo(false)
@@ -32,15 +87,58 @@ func TestExporter(t *testing.T) {
 	defer backend.Close()
 	interrupt.RegisterCleaner(backend.Close)
 
-	token := os.Getenv("GITHUB_TOKEN_PRIVATE")
-	if token == "" {
-		t.Skip("Env var GITHUB_TOKEN_PRIVATE missing")
+	bugs, err := testCases(backend)
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	tests := []struct {
+		name       string
+		operations []bug.Operation
+	}{
+		{
+			name:       "simple bug",
+			operations: bugs["simple bug"].Snapshot().Operations,
+		},
+		{
+			name:       "bug with comments",
+			operations: bugs["bug with comments"].Snapshot().Operations,
+		},
+		{
+			name:       "bug label change",
+			operations: bugs["bug change label"].Snapshot().Operations,
+		},
+		/*
+			{
+				name: "bug status edition",
+			},
+			{
+				name: "bug title edition",
+			},
+			{
+				name: "bug comments edition",
+			},
+			{
+				name: "complex bug",
+			},
+		*/
+	}
+	// generate project name
+	projectName := generateRepoName()
+	if err := createRepository(projectName, token); err != nil {
+		t.Fatal(err)
+	}
+
+	defer func() {
+		if err := deleteRepository(projectName, user, token); err != nil {
+			t.Fatal(err)
+		}
+	}()
 
 	exporter := &githubExporter{}
 	err = exporter.Init(core.Configuration{
-		keyOwner:   "MichaelMure",
-		keyProject: "git-bug-exporter-tests",
+		keyOwner:   user,
+		keyProject: projectName,
 		keyToken:   token,
 	})
 	require.NoError(t, err)
@@ -51,7 +149,6 @@ func TestExporter(t *testing.T) {
 	require.NoError(t, err)
 
 	fmt.Printf("test repository exported in %f seconds\n", time.Since(start).Seconds())
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
@@ -59,14 +156,75 @@ func TestExporter(t *testing.T) {
 	}
 }
 
-func genRepoName() {}
+func generateRepoName() string {
+	rand.Seed(time.Now().UnixNano())
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, 8)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return fmt.Sprintf("%s-%s", testRepoBaseName, string(b))
+}
 
-func createRepository() {}
+func createRepository(project, token string) error {
+	url := fmt.Sprintf("%s/user/repos", githubV3Url)
 
-func deleteRepository() {}
+	params := struct {
+		Name        string `json:"name"`
+		Description string `json:"description"`
+		Private     bool   `json:"private"`
+		HasIssues   bool   `json:"has_issues"`
+	}{
+		Name:        project,
+		Description: "git-bug exporter temporary test repository",
+		Private:     true,
+		HasIssues:   true,
+	}
 
-// verifyIssue is full
-// comments
-func verifyIssue() {
+	data, err := json.Marshal(params)
+	if err != nil {
+		return err
+	}
 
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+
+	// need the token for private repositories
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", token))
+
+	client := &http.Client{
+		Timeout: defaultTimeout,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return resp.Body.Close()
+}
+
+func deleteRepository(project, owner, token string) error {
+	url := fmt.Sprintf("%s/repos/%s/%s", githubV3Url, owner, project)
+
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+
+	// need the token for private repositories
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", token))
+
+	client := &http.Client{
+		Timeout: defaultTimeout,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return resp.Body.Close()
 }
